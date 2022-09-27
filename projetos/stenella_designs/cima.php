e5729cd35229fc6e87bed71bcaabe5452daed2aa
joaopaulom_dev <joaopaulomsilva2021@outlook.com>
2022-09-27 09:11:41 -0300

    first commit

---
+++ projetos/stenella_designs/cima.php
@@ -0,0 +1,23 @@
+<?php
+//Pega pastas
+$imagens = 'dist/images/';
+$estilo = 'dist/css/';
+$scripts = 'dist/js/';
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+	<meta name="author" content="@joaopaulom_dev" />
+	<meta name="description" content="Stenella, designs inovadores e inteligentes" />
+	<meta name="keywords" content="Design,Design inovador,Design inteligente," />
+	<title>Stenella Designs</title>
+	
+	<!-- Estilo website -->
+	<link rel="stylesheet" type="text/css" href="dist/css/style.css" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
---
+++ projetos/stenella_designs/index.php
@@ -0,0 +1,17 @@
+<?php include 'cima.php' ?>
+<div id="container">
+	<main>
+		<header id="main-header" class="relative">
+			<img src="dist/images/logo.png" alt="Um golfinho, símbolo do website" width="75" />
+			<nav class="nav-link">
+				<a href="#">Serviços</a>
+				<a href="#">Insights</a>
+				<a href="#">Sobre</a>
+			</nav>
+			<nav class="nav-link">
+				<a href="#" class="call-to-action">Contato</a>
+			</nav>
+		</header>
+	</main>
+</div>
+<?php include 'baixo.php' ?>
