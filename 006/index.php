d255bd9f6cc2608e4a72fdafbc1b44065055d357
joaopaulom_dev <joaopaulomsilva2021@outlook.com>
2022-09-30 09:50:52 -0300

    Criando efeitos com 'animation'

---
+++ 006/index.php
@@ -0,0 +1,10 @@
+<?php 
+$p = 'Criando animações com \'transition\'';
+include_once '../cima.php'; 
+?>
+<div class="container">
+	<button type="button" class="action-button">Enviar</button>
+	<button type="button" class="action-button2">Enviar</button>
+	<button type="button" class="action-button3">Enviar</button>
+</div>
+<?php include_once '../baixo.php' ?>
---
+++ 007/index.php
@@ -0,0 +1,11 @@
+<?php 
+$p = 'Criando efeitos com \'animation\'';
+include_once '../cima.php'; 
+?>
+<div class="container">
+	<div class="loading-box">
+		<span>Carregando...</span>
+		<hr />
+	</div>
+</div>
+<?php include_once '../baixo.php' ?>
