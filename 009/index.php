ba033ff78275dadd814c3011fb30a7e92b60dbd0
joaopaulom_dev <joaopaulomsilva2021@outlook.com>
2022-10-01 18:03:53 -0300

    Dividindo texto através de 'columns'

---
+++ 009/index.php
@@ -0,0 +1,30 @@
+<?php 
+$p = 'Implementando fonts com \'font-face\'';
+include_once '../cima.php'; 
+?>
+<div class="container">
+	<div class="box">
+		<p class="paragrafo_01">
+			Ao contrário da crença popular, o Lorem Ipsum não é simplesmente texto aleatório. 
+			Tem raízes numa peça de literatura clássica em Latim, de 45 AC, tornando-o com mais de 2000 anos. 
+			Richard McClintock, um professor de Latim no Colégio Hampden-Sydney, na Virgínia, procurou uma das palavras 
+			em Latim mais obscuras (consectetur) numa passagem Lorem Ipsum, e atravessando as cidades do mundo 
+			na literatura clássica, descobriu a sua origem. Lorem Ipsum vem das secções 1.10.32 e 1.10.33 
+			do "de Finibus Bonorum et Malorum" (Os Extremos do Bem e do Mal), por Cícero, escrito a 45AC. 
+			Este livro é um tratado na teoria da ética, muito popular durante a Renascença. 
+			A primeira linha de Lorem Ipsum, "Lorem ipsum dolor sit amet..." aparece de uma linha na secção 1.10.32.
+		</p>
+	</div>
+</div>
+<!--
+<p class="paragrafo_02">
+	Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, 
+	mantendo-se essencialmente inalterada.
+</p>
+<p class="paragrafo_03">
+	Foi popularizada nos anos 60 com a disponibilização das folhas de Letraset, que continham 
+	passagens com Lorem Ipsum, e mais recentemente com os programas de publicação como o Aldus PageMaker 
+	que incluem versões do Lorem Ipsum.
+</p>
+-->
+<?php include_once '../baixo.php' ?>
