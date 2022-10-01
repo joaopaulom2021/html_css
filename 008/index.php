0bdaae5c8106420a21158ed4e221a6c11d57c292
joaopaulom_dev <joaopaulomsilva2021@outlook.com>
2022-10-01 06:01:14 -0300

    Subpropriedades do 'background' | 'background-clip'

---
+++ 008/index.php
@@ -0,0 +1,10 @@
+<?php 
+$p = 'Subpropriedades do \'background\'';
+include_once '../cima.php'; 
+?>
+<div class="container">
+	<div class="box">background-clip:padding-box</div>
+	<div class="box">background-clip:content-box</div>
+	<div class="box">background-clip:border-box</div>
+</div>
+<?php include_once '../baixo.php' ?>
