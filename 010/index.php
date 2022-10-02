aba1991e5ceb33a2ee18166352afb0ec22226183
joaopaulom_dev <joaopaulomsilva2021@outlook.com>
2022-10-02 14:13:03 -0300

    Introduzindo videos com a tag 'video'

---
+++ 010/index.php
@@ -0,0 +1,10 @@
+<?php 
+$p = 'Introduzindo videos com a tag \'video\'';
+include_once '../cima.php'; 
+?>
+<div class="container">
+	<div class="box">
+		<video src="../../videos/001.mp4" width="1000" poster="../../images/268l9I-wqC4_1280.jpg" />
+	</div>
+</div>
+<?php include_once '../baixo.php' ?>
